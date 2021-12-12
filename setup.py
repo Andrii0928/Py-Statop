b6ad50acb5492d59b10b95192aaf810103208cb0
Vladyslav092 <katli90210@gmail.com>
2021-12-12 08:29:02 +0300

    Update Version

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='statopt',
+    version='0.2',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Statistical adaptive stochastic optimization methods',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=['numpy', 'scipy', 'torch'],
+    url='https://github.com/microsoft/statopt'
+)
